f26ea7ca2eb766ba26824872cd1278a8bcc743f2
trevornei <117675657+trevornei@users.noreply.github.com>
2023-12-01 15:25:04 -0700

    Added a nav component, images, svgs, and component folders.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Nav from './components/nav/Nav'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-       Hello world!
-      </h1>
+      
     </>
   )
 }
-
-export default App
