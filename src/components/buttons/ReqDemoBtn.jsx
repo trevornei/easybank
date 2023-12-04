8bb72d8901f9415f88ff5826c7ee29363e6d35da
trevornei <117675657+trevornei@users.noreply.github.com>
2023-12-04 11:42:20 -0700

    Commit.

--- src/components/buttons/ReqDemoBtn.jsx
+++ src/components/buttons/ReqDemoBtn.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function() {
     return(
         <>
-            <div className="flex flex-row items-center justify-center w-40 bg-green-300 h-11 rounded-xl">
+            <div className="flex flex-row items-center justify-center w-40 h-12 px-12 bg-green-300 bg-gradient-to-br from-green-500 to-cyan-500 rounded-3xl">
                 <button>
-                    <h3>Request Demo</h3>
+                    <h3 className="px-4">Request Demo</h3>
                 </button>
             </div>
         </>
