219094d500b791f4bfafb4c747c0f598bfbd80c2
trevornei <117675657+trevornei@users.noreply.github.com>
2023-12-06 10:53:38 -0700

    Changed text color & padding for navigation links. Then, fixed Request invite styling.

--- src/components/buttons/ReqDemoBtn.jsx
+++ src/components/buttons/ReqDemoBtn.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function() {
     return(
         <>
-            <div className="flex flex-row items-center justify-center w-40 h-12 px-12 bg-green-300 bg-gradient-to-br from-green-500 to-cyan-500 rounded-3xl">
+            <div className="flex flex-row items-center justify-center h-12 bg-green-300 w-44 bg-gradient-to-br from-green-500 to-cyan-500 rounded-3xl">
                 <button>
-                    <h3 className="px-4">Request Demo</h3>
+                    <h3 className="px-12 font-semibold text-white whitespace-nowrap">Request Invite</h3>
                 </button>
             </div>
         </>
--- src/components/nav/pagesUL.jsx
+++ src/components/nav/pagesUL.jsx
@@ -4,20 +4,20 @@ export default function() {
     return(
         <>
             <div className="">
-                <ul className="flex flex-row">
-                    <li>
+                <ul className="flex flex-row font-light text-mGrey">
+                    <li className="px-4">
                         <p>Careers</p>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <p>About</p>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <p>Contact</p>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <p>Blog</p>
                     </li>
-                    <li>
+                    <li className="px-4">
                         <p>Careers</p>
                     </li>
                 </ul>
