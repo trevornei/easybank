d1dadff20a511c56d2d228531ade2418b3e3837d
trevornei <117675657+trevornei@users.noreply.github.com>
2024-03-22 19:28:36 -0700

    Fixed imgae height for card three, landing page, xl to w-64 h-48.

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Article({ img, author, title, description }) {
   return (
-    <div className="flex flex-col items-center justify-center bg-white xl:w-full xl:h-full rounded-t-xl xl:mx-4">
-        <img className='rounded-t-lg shadow-sm xl:h-auto xl:w-full h-52 shadow-darkBlue/20' src={img}/>
-        <div className="items-center xl:px-2 justify-normal">
-          <h3 className="">{title}</h3>
-          <p className=''>{description}</p>
-          <p className="font-semi text-darkBlue/40">By: {author}</p>
+    <div className="flex flex-col items-center justify-center bg-white xl:h-full xl:w-full rounded-t-xl xl:mx-4">
+        <img className='w-64 h-48 rounded-t-lg shadow-darkBlue/20' src={img}/>
+        <div className="items-center xl:p-2 xl:px-2 justify-normal">
+          <p className="xl:p-2 font-semi text-darkBlue/40">By: {author}</p>
+          <h3 className="xl:p-2 hover:text-eCash hover:delay-300 hover:transition-colors hover:ease-in-out">{title}</h3>
+          <p className='xl:p-2 '>{description}</p>
         </div>
     </div>
   )
--- src/components/cardThree/CardThree.jsx
+++ src/components/cardThree/CardThree.jsx
@@ -17,23 +17,30 @@ Conffetti
 export default function CardThree() {
 
   let moneyTitle = 'Recieve Money in Any Currency with No Fees.'
-  let moneyDesc = 'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …'
+  let moneyDesc = 'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single…'
   let moneyAuthor = 'Claire Robinson'
 
   let restTitle = 'Treat Yourself Without Worrying About Money'
-  let restDesc = 'Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …'
+  let restDesc = 'Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you…'
   let restAuthor = 'Wilson Hutson'
-  
+
+  let travelTitle = 'Take you Easybank card wherever you go'
+  let travelDesc = 'We want you to enjoy your travels. This is why we don\'t charge any fees on purchases while you\'re abroad. We\'ll even show you...'
+  let travelAuthor = 'Claire Robinson'
+
+  let betaTitle = ' Our invite-only Beta accounts are now live!'
+  let betaDesc = 'After a lot of hard work by the whole team, we\re excited to launch our closed beta. It\'s easy to request an invite through the site...'
+
   return (
     <>
-        <div className="xl:px-40">
-          <div className="xl:px-28 xl:pt-16">
+        <div className="flex flex-col items-start justify-start xl:px-40 bg-mGrey/10">
+          <div className="xl:mx-4 xl:pt-16">
             <h1 className="text-6xl text-darkBlue">Latest Articles</h1>
           </div>
-          <div className="flex flex-row items-center justify-between xl:px-28 xl:pb-32 xl:pt-10">
+          <div className="flex flex-row items-start justify-between xl:w-full xl:h-full xl:pb-32 xl:pt-10">
               <Article img={MoneyPhoto} title={moneyTitle} description={moneyDesc} author={moneyAuthor} />
               <Article img={RestaurantPhoto} title={restTitle} description={restDesc} author={restAuthor} />
-              <Article img={planePhoto} title={moneyTitle} description={moneyDesc} author={moneyAuthor} />
+              <Article img={planePhoto} title={travelTitle} description={travelDesc} author={moneyAuthor} />
               <Article img={confettiPhoto} title={moneyTitle} description={moneyDesc} author={moneyAuthor} />
           </div>
         </div>
