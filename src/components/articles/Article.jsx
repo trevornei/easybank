ba1b0f33162fbf9b8f28180ab5cb6551a08741d7
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-28 14:01:24 -0700

    fixed the height and justify-content for the elements nested in cardThree at the xl breakpoint.

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Article({ img, author, title, description }) {
   return (
-    <div className="flex flex-col items-center justify-center bg-white xl:h-full xl:w-full rounded-t-xl xl:mx-4">
-        <img className='w-64 h-48 rounded-t-lg shadow-darkBlue/20' src={img}/>
-        <div className="items-center xl:p-2 xl:px-2 justify-normal">
-          <p className="xl:p-2 font-semi text-darkBlue/40">By: {author}</p>
-          <h3 className="text-2xl font-bold xl:p-2 hover:text-eCash/70 hover:duration-300 text-darkBlue hover:transition-colors hover:ease-in-out">{title}</h3>
-          <p className='xl:p-2 '>{description}</p>
-        </div>
+    <div className="flex flex-col items-center justify-start bg-white xl:w-[500px] xl:h-[700px] rounded-t-xl xl:mx-4">
+      <img
+        className="w-96 h-auto aspect-square rounded-t-lg shadow-darkBlue/20"
+        src={img}
+      />
+      <div className="items-center xl:p-2 xl:px-2 justify-normal">
+        <p className="xl:p-2 font-semi text-darkBlue/40">By: {author}</p>
+        <h3 className="text-2xl font-bold xl:p-2 hover:text-eCash/70 hover:duration-300 text-darkBlue hover:transition-colors hover:ease-in-out">
+          {title}
+        </h3>
+        <p className="xl:p-2 ">{description}</p>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/cardThree/CardThree.jsx
+++ src/components/cardThree/CardThree.jsx
@@ -1,49 +1,76 @@
-import React from 'react'
-import Article from '../articles/Article'
+import React from "react";
+import Article from "../articles/Article";
 
 // Images
-import MoneyPhoto from '../../assets/img/image-currency.jpg'
-import RestaurantPhoto from '../../assets/img/image-restaurant.jpg'
-import planePhoto from '../../assets/img/image-plane.jpg'
-import confettiPhoto from '../../assets/img/image-confetti.jpg'
+import MoneyPhoto from "../../assets/img/image-currency.jpg";
+import RestaurantPhoto from "../../assets/img/image-restaurant.jpg";
+import planePhoto from "../../assets/img/image-plane.jpg";
+import confettiPhoto from "../../assets/img/image-confetti.jpg";
 
-{/*
+{
+  /*
 Money,
 Dinner,
 Plane, 
 Conffetti
-*/}
+*/
+}
 
 export default function CardThree() {
+  let moneyTitle = "Recieve Money in Any Currency with No Fees.";
+  let moneyDesc =
+    "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single…";
+  let moneyAuthor = "Claire Robinson";
 
-  let moneyTitle = 'Recieve Money in Any Currency with No Fees.'
-  let moneyDesc = 'The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single…'
-  let moneyAuthor = 'Claire Robinson'
+  let restTitle = "Treat Yourself Without Worrying About Money";
+  let restDesc =
+    "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you…";
+  let restAuthor = "Wilson Hutson";
 
-  let restTitle = 'Treat Yourself Without Worrying About Money'
-  let restDesc = 'Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you…'
-  let restAuthor = 'Wilson Hutson'
+  let travelTitle = "Take you Easybank card wherever you go";
+  let travelDesc =
+    "We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you...";
+  let travelAuthor = "Claire Robinson";
 
-  let travelTitle = 'Take you Easybank card wherever you go'
-  let travelDesc = 'We want you to enjoy your travels. This is why we don\'t charge any fees on purchases while you\'re abroad. We\'ll even show you...'
-  let travelAuthor = 'Claire Robinson'
-
-  let betaTitle = ' Our invite-only Beta accounts are now live!'
-  let betaDesc = 'After a lot of hard work by the whole team, we\re excited to launch our closed beta. It\'s easy to request an invite through the site...'
+  let betaTitle = " Our invite-only Beta accounts are now live!";
+  let betaDesc =
+    "After a lot of hard work by the whole team, we\re excited to launch our closed beta. It's easy to request an invite through the site...";
 
   return (
     <>
-        <div className="flex flex-col items-start justify-start xl:px-40 bg-mGrey/10">
-          <div className="xl:mx-4 xl:pt-16">
-            <h1 className="text-6xl text-darkBlue">Latest Articles</h1>
-          </div>
-          <div className="flex flex-row items-start justify-between xl:w-full xl:h-full xl:pb-32 xl:pt-10">
-              <Article img={MoneyPhoto} title={moneyTitle} description={moneyDesc} author={moneyAuthor} />
-              <Article img={RestaurantPhoto} title={restTitle} description={restDesc} author={restAuthor} />
-              <Article img={planePhoto} title={travelTitle} description={travelDesc} author={moneyAuthor} />
-              <Article img={confettiPhoto} title={moneyTitle} description={moneyDesc} author={moneyAuthor} />
-          </div>
+      <div className="flex flex-col items-start justify-start xl:px-16 bg-mGrey/10">
+        <div className="xl:mx-4 xl:pt-16">
+          <h1 className="text-3xl tracking-wide text-darkBlue">
+            Latest Articles
+          </h1>
         </div>
+        <div className="flex flex-row items-start justify-between xl:w-full xl:h-full xl:pb-32 xl:pt-10">
+          <Article
+            img={MoneyPhoto}
+            title={moneyTitle}
+            description={moneyDesc}
+            author={moneyAuthor}
+          />
+          <Article
+            img={RestaurantPhoto}
+            title={restTitle}
+            description={restDesc}
+            author={restAuthor}
+          />
+          <Article
+            img={planePhoto}
+            title={travelTitle}
+            description={travelDesc}
+            author={moneyAuthor}
+          />
+          <Article
+            img={confettiPhoto}
+            title={moneyTitle}
+            description={moneyDesc}
+            author={moneyAuthor}
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
