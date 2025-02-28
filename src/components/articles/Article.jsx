e837ac0f18452670514059fb2d73ed63aade95f6
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-28 14:23:01 -0700

    Fixed styling for the text in the <Article /> componenet in cardThree.jsx

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -2,18 +2,22 @@ import React from "react";
 
 export default function Article({ img, author, title, description }) {
   return (
-    <div className="flex flex-col items-center justify-start bg-white xl:w-[500px] xl:h-[700px] rounded-t-xl xl:mx-4">
-      <img
-        className="w-96 h-auto aspect-square rounded-t-lg shadow-darkBlue/20"
-        src={img}
-      />
-      <div className="items-center xl:p-2 xl:px-2 justify-normal">
-        <p className="xl:p-2 font-semi text-darkBlue/40">By: {author}</p>
-        <h3 className="text-2xl font-bold xl:p-2 hover:text-eCash/70 hover:duration-300 text-darkBlue hover:transition-colors hover:ease-in-out">
-          {title}
-        </h3>
-        <p className="xl:p-2 ">{description}</p>
+    <>
+      <div className="flex flex-col items-center justify-start overflow-clip bg-white xl:w-[500px] xl:h-[550px] rounded-t-xl xl:mx-4">
+        <img
+          className="xl:w-full h-auto aspect-square rounded-t-lg shadow-darkBlue/20"
+          src={img}
+        />
+        <div className="items-center justify-normal xl:py-2">
+          <p className="font-semi xl:text-sm xl:px-2 text-darkBlue/40">
+            By: {author}
+          </p>
+          <h3 className="xl:text-lg font-normal xl:p-2 hover:text-eCash/70 hover:duration-300 text-darkBlue hover:transition-colors hover:ease-in-out">
+            {title}
+          </h3>
+          <p className="xl:p-2 xl:text-sm text-slate-500">{description}</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
