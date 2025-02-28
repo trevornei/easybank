8f29cb3520abb96a90aaa3c537beee2915b9536a
trevornei <117675657+trevornei@users.noreply.github.com>
2025-02-28 12:17:28 -0700

    Catching up commits

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <>
       <div className="xl:pt-40 xl:pb-0 xl:px-0 flex-wrap flex-row max-w-screen max-h-[676px] relative z-20 xl:w-auto">
-        <div className="z-50 flex-col flex-wrap items-start justify-center h-auto text-left xl:-translate-x-20 max-w-96 xl:ml-28  bg-eCash/30 backdrop-blur-2xl xl:w-[600px] xl:p-8 xl:rounded-lg">
+        <div className="z-50 flex-col flex-wrap items-start justify-center h-auto text-left xl:-translate-x-20 max-w-96 xl:ml-28  bg-eCash/10 backdrop-blur-2xl xl:w-[600px] xl:p-8 xl:rounded-lg">
           <h1 className="text-5xl leading-[64px] text-darkBlue xl:font-bold">
             Next Generation
           </h1>
